f12c1916fc919dd3b0a0f26ae3d3e2eff60795a2
Jennifer Adrianne I. Caballes <yan4414@gmail.com>
2021-10-09 13:23:02 +0800

    Create ex_xml.py

---
+++ ex_xml.py
@@ -0,0 +1,23 @@
+
+import urllib.request as ur
+import xml.etree.ElementTree as ET
+
+url = input("Enter location: ")
+if len(url) < 1:
+    url = "http://python-data.dr-chuck.net/comments_200531.xml"
+print ("Retrieving ", url)
+
+xml = ur.urlopen(url).read()
+print ("Retrieved: " + str(len(xml)) + " characters")
+
+tree = ET.fromstring(xml)
+
+counts =  tree.findall('.//count')
+print ("Count: " + str(len(counts)))
+
+accumulator = 0
+
+for count in counts:
+    accumulator += int(count.text)
+
+print ("Sum:" + str(accumulator))
